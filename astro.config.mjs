e31af6183ca88e5ee5455a531489b0f847d5948c
Ayrton Teniente <ayrton.94.e@gmail.com>
2026-09-15 22:15:38 -0600

    fix: remove unused configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  redirects: {
-    "/[...path]": "https://itonx.dev/codealchemy",
-  },
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
